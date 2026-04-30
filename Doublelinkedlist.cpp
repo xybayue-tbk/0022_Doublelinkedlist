f2fa9d329a725150c81b29070cf504d78082e7e1
xybayue-tbk <xybayue@gmail.com>
2026-04-30 21:09:14 +0700

    Menambahkan Fungsi void traverse

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -124,4 +124,27 @@ public:
         delete current;
         cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
+
+
+//Batas
+    void traverse(){
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        Node *currentNode = START;
+
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            currentNode = currentNode->next;
+            i++;
+        }
+    }
+
 };
