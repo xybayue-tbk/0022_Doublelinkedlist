4b5dba94e53878b75ad0b70415227dd14c62c75a
xybayue-tbk <xybayue@gmail.com>
2026-04-30 21:16:05 +0700

    Menambahkan void revtraverse

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -147,4 +147,30 @@ public:
         }
     }
 
+        void revtraverse()
+        {
+        if (START == NULL)
+        {
+        cout << "\nList is empty" << endl;
+            return;
+        }
+
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+            currentNode = currentNode->prev;
+            i--;
+        }
+        }
+
+
 };
