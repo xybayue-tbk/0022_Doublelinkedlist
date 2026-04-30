17f0affd80e1ee7cfd9a684f067faf9fe5caf612
xybayue-tbk <xybayue@gmail.com>
2026-04-30 20:47:36 +0700

    void addNode dan langkak 1,2

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -20,4 +20,16 @@ public:
     {
         START = NULL;
     }
+
+    void addNode(){
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+        
+       // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+    }
 };
