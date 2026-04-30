fdf34200fd5cdea163cfec7a0a25664c27fef8f2
xybayue-tbk <xybayue@gmail.com>
2026-04-30 20:59:18 +0700

    Melanjutkan yang sebelumnya

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -87,5 +87,21 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+        
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 };
